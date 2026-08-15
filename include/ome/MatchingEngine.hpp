f27ba41f0c6ecd879ae93d58da1476f1ceefbbb4
Benjamin Zou <bwz5@cornell.edu>
2026-08-15 13:23:28 -0700

    Creating the Matching Engine header

---
+++ include/ome/MatchingEngine.hpp
@@ -0,0 +1,44 @@
+#ifndef MATCHING_ENGINE_HPP
+#define MATCHING_ENGINE_HPP
+
+#include <optional>
+#include <string>
+#include <unordered_map>
+
+#include "OrderBook.hpp"
+
+namespace ome {
+using Ticker = std::string;
+
+class MatchingEngine {
+ private:
+  std::unordered_map<Ticker, ome::OrderBook> ticker_to_orderBook;
+
+ public:
+  // Returns "true" if the update was successful, "false" otherwise
+  bool updateOrder(const Ticker& ticker, ome::OrderId orderId,
+                   ome::Quantity quantity, ome::Price price);
+
+  // Returns "true" if the cancel was sucessful, "false" otherwise
+  bool cancelOrder(const Ticker& ticker, ome::OrderId orderId);
+
+  // Returns the unique orderId for this specific order. Fulfills the order as
+  // much as possible.
+  ome::OrderId placeOrder(const Ticker& ticker, ome::Price _p, ome::Quantity _q,
+                          ome::OrderSide _oS);
+
+  // Returns the lowest active Ask order if one exists
+  std::optional<ome::Order> getBestAsk(const Ticker& ticker) const;
+
+  // Returns the highest active Bid order if one exists
+  std::optional<ome::Order> getBestBid(const Ticker& ticker) const;
+
+  // Returns the total Ask quantity arcross all active Ask orders
+  ome::Quantity getTotalAskQuantity(const Ticker& ticker) const;
+
+  // Returns the total Bid quantity across all active Bid orders
+  ome::Quantity getTotalBidQuantity(const Ticker& ticker) const;
+};
+}  // namespace ome
+
+#endif
